8d262361fafd291f6f2113305f042aaca5ac04cb
ryan@teak <ryan@teak>
2010-02-23 07:31:41 -0500

    Minor code cleanup

--- core/src/main/java/com/thimbleware/jmemcached/protocol/binary/MemcachedBinaryPipelineFactory.java
+++ core/src/main/java/com/thimbleware/jmemcached/protocol/binary/MemcachedBinaryPipelineFactory.java
@@ -10,24 +10,12 @@ import org.jboss.netty.channel.group.DefaultChannelGroup;
 
 public class MemcachedBinaryPipelineFactory implements ChannelPipelineFactory {
 
-    private Cache cache;
-    private String version;
-    private boolean verbose;
-    private int idleTime;
-
-    private DefaultChannelGroup channelGroup;
-
     private final MemcachedBinaryCommandDecoder decoder =  new MemcachedBinaryCommandDecoder();
     private final MemcachedCommandHandler memcachedCommandHandler;
     private final MemcachedBinaryResponseEncoder memcachedBinaryResponseEncoder = new MemcachedBinaryResponseEncoder();
 
     public MemcachedBinaryPipelineFactory(Cache cache, String version, boolean verbose, int idleTime, DefaultChannelGroup channelGroup) {
-        this.cache = cache;
-        this.version = version;
-        this.verbose = verbose;
-        this.idleTime = idleTime;
-        this.channelGroup = channelGroup;
-        memcachedCommandHandler = new MemcachedCommandHandler(this.cache, this.version, this.verbose, this.idleTime, this.channelGroup);
+        memcachedCommandHandler = new MemcachedCommandHandler(cache, version, verbose, idleTime, channelGroup);
     }
 
     public ChannelPipeline getPipeline() throws Exception {
